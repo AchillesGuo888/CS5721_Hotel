8475f15a573d5fa874cca8c3f25b0ab1e8486dc0
Pockyboo <barisengin1208@gmail.com>
2024-11-09 12:05:06 +0000

    room dto additions2

--- src/main/java/com/example/hotel/dto/request/AddRoomRequestDTO.java
+++ src/main/java/com/example/hotel/dto/request/AddRoomRequestDTO.java
@@ -2,14 +2,35 @@ package com.example.hotel.dto.request;
 
 import com.example.hotel.dto.request.Base.Request;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @ToString
 @ApiModel("add room request parameter")
 public class AddRoomRequestDTO extends Request {
-
+    @ApiModelProperty(value = "hotel id",required = true, example = "01")
+    private int hotelId;
+    @ApiModelProperty(value = "room type id",required = true, example = "01")
+    private int roomTypeId;
+    @ApiModelProperty(value = "update time",required = true, example = "01/01/2025")
+    private LocalDateTime updateTime;
+    @ApiModelProperty(value = "create time",required = true, example = "01/01/2025")
+    private LocalDateTime createTime;
+    @ApiModelProperty(value = "is deleted",required = true, example = "0")
+    private int isDeleted;
+    @ApiModelProperty(value = "room number",required = true, example = "42")
+    private String roomNumber;
+    @ApiModelProperty(value = "room key",required = true, example = "51544")
+    private int roomKey;
+    
 }
+
+
+
+
--- src/main/java/com/example/hotel/dto/request/ChangeRoomRequestDTO.java
+++ src/main/java/com/example/hotel/dto/request/ChangeRoomRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.hotel.dto.request;
 
 import com.example.hotel.dto.request.Base.Request;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -12,5 +13,6 @@ import lombok.ToString;
 @ApiModel("change room request parameter")
 
 public class ChangeRoomRequestDTO extends Request {
-
+    @ApiModelProperty(value = "room id",required = true, example = "01")
+    private int roomId;
 }
