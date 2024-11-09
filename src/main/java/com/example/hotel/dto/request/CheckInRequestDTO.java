419ef60ec70ea0f64a968a3eb313f6fbe1d190da
Pockyboo <barisengin1208@gmail.com>
2024-11-09 11:38:15 +0000

    room dto additions

--- src/main/java/com/example/hotel/dto/request/CheckInRequestDTO.java
+++ src/main/java/com/example/hotel/dto/request/CheckInRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.hotel.dto.request;
 
 import com.example.hotel.dto.request.Base.Request;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -11,5 +12,6 @@ import lombok.ToString;
 @ToString
 @ApiModel("cancel order request parameter")
 public class CheckInRequestDTO extends Request {
-
+    @ApiModelProperty(value = "room id",required = true, example = "01")
+    private int roomId;
 }
--- src/main/java/com/example/hotel/dto/request/DeleteRoomRequestDTO.java
+++ src/main/java/com/example/hotel/dto/request/DeleteRoomRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.hotel.dto.request;
 
 import com.example.hotel.dto.request.Base.Request;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -11,5 +12,6 @@ import lombok.ToString;
 @ToString
 @ApiModel(value = "delete room info parameter")
 public class DeleteRoomRequestDTO extends Request {
-
+    @ApiModelProperty(value = "room id",required = true, example = "01")
+    private int roomId;
 }
