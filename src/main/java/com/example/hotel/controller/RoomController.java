585c60872e8d552dfa1908ea092d8f34574088d6
GuoRui <660425@gdpr.com>
2024-12-05 18:49:47 +0000

    settle requestbody null value

--- src/main/java/com/example/hotel/controller/RoomController.java
+++ src/main/java/com/example/hotel/controller/RoomController.java
@@ -19,7 +19,7 @@ import com.example.hotel.exception.BizException;
 import com.example.hotel.util.JwtUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -29,6 +29,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
